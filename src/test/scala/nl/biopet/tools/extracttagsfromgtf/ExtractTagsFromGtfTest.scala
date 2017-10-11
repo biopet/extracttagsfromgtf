702a13eb0da568778279a88becfb657d370d5535
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:37 +0200

    object to class

--- src/test/scala/nl/biopet/tools/extracttagsfromgtf/ExtractTagsFromGtfTest.scala
+++ src/test/scala/nl/biopet/tools/extracttagsfromgtf/ExtractTagsFromGtfTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.extracttagsfromgtf
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object ExtractTagsFromGtfTest extends BiopetTest {
+class ExtractTagsFromGtfTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {
